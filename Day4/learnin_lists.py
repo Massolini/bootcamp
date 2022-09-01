795b8b87b9b57f5c81edb6faad5df0b8c3327923
Mauro Massolini Silva <massolini@gmail.com>
2022-09-01 11:14:53 +0200

    adicionado exercicio 4.2 e arquivo de listas

---
+++ Day4/learnin_lists.py
@@ -0,0 +1,13 @@
+#Lists initiate always with [ ]
+
+provincias_de_espana = ["Álava","Navarra","La Rioja","Guipuzcoa"]
+
+provincias_de_espana.append("Albacete") #(input("Digite el nombre de la provincia para añadirla: "))
+provincias_de_espana.sort()
+
+print(provincias_de_espana)
+
+#append - adiciona um item no final da lista
+# sort - classifica o modo que quer clasificar as informacoes
+# https://docs.python.org/3/library/stdtypes.html#lists
+
