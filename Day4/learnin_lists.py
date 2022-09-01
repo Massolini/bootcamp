62c14553d0af0a8c5f6f76e31da5461f73f6ac37
Mauro Massolini Silva <massolini@gmail.com>
2022-09-01 13:05:35 +0200

    finalizado exercicio treasure map

--- Day4/learnin_lists.py
+++ Day4/learnin_lists.py
@@ -11,3 +11,17 @@ print(provincias_de_espana)
 # sort - classifica o modo que quer clasificar as informacoes
 # https://docs.python.org/3/library/stdtypes.html#lists
 
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# fruits[-1] = "Melons" # quando adiciona -1 se lee de tras para frente comecando pelo -1
+# fruits.append("Lemons")
+
+# print(fruits) 
+
+
+
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+# dirty_dozen = [fruits, vegetables]
+
+# print(dirty_dozen[0][1]) # o primeiro [] é para identificar se é fruta ou vegetal e o segundo [] é para os ingredientes
---
+++ Day4/treasure_map.py
@@ -0,0 +1,18 @@
+from calendar import c
+
+
+row1 = ["#","#","#"]
+row2 = ["#","#","#"]
+row3 = ["#","#","#"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")  # se utiliza o \n para separar cada linha e deixar uma embaixo da outra  \n se usa para nova linha
+position = input("Where do you want to put the treasure? ") # 23 = column 2, row 3 
+
+column = int(position[0]) # vai selecionar o primeiro caracter e transformarlo em int
+row = int(position[1]) # vai selecionar o segundo caracter e transformarlo em int
+
+select_row = map[row -1]
+select_row [column -1] = "X"
+
+
+print(f"{row1}\n{row2}\n{row3}")
