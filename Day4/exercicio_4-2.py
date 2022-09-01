4540a648eee613e5c97b6bb060ce7c0a352f81b8
Mauro Massolini Silva <massolini@gmail.com>
2022-09-01 11:18:02 +0200

    modificado o nome da string

--- Day4/exercicio_4-2.py
+++ Day4/exercicio_4-2.py
@@ -4,8 +4,8 @@ names = names_string.split(",")
 numero_de_pessoas = len(names) - 1 
 # O -1 para é nao passar do range ex. se o numero da lista sao 5 pessoas a lista conta de zero 0 a quatro 4 sem o -1 apareceria o 5 e da erro por nao existir na lista
 
-random_chosen = random.randint(0, numero_de_pessoas)
+random_choice = random.randint(0, numero_de_pessoas)
 
-print(random_chosen) #mostra o numero da lista gerado
+print(random_choice) #mostra o numero da lista gerado
 
 print(f"O nome escolhido para pagar a conta é: {names[random_chosen]}")
