093fc4cfa1b8882c5c2a0d9d027ee9096006eac5
Mauro Massolini Silva <massolini@gmail.com>
2022-09-02 11:16:37 +0200

    changes to draw, set it all in one if

--- Day4/rock_papper_scissors.py
+++ Day4/rock_papper_scissors.py
@@ -6,7 +6,7 @@ oponent_choice = random.randint(0,2)
 
 print(f"\nPlayer choice: {user_choice} >< Computer choice: {oponent_choice}\n" )
 
-if user_choice == 0 and oponent_choice == 0:
+if user_choice == oponent_choice:
     print("It\'s a draw! everebody wins :)\n")
 elif user_choice == 0 and oponent_choice == 1:
     print("Paper wraps rock. Computer win!\n")
@@ -14,15 +14,11 @@ elif user_choice == 0 and oponent_choice == 2:
     print("Rock crushes scissors! You win!\n")
 elif user_choice == 1 and oponent_choice == 0:
     print("Paper wraps rock. You win!\n")
-elif user_choice == 1 and oponent_choice == 1:
-    print("It\'s a draw! everebody wins :)\n")
 elif user_choice == 1 and oponent_choice == 2:
     print("Scissors cuts paper! Computer win!\n")
 elif user_choice == 2 and oponent_choice == 0:
     print("Rock crushes scissors! Computer win!\n")
 elif user_choice == 2 and oponent_choice == 1:
     print("Scissors cuts paper! You win!\n")
-elif user_choice == 2 and oponent_choice == 2:
-    print("It\'s a draw! everebody wins :)\n")
 else:
     print("chose a number betwen 0, 1 or 2\n")
