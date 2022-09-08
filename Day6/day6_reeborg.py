ce49057b2ab6df2a81c73fb071340254f661a83b
Mauro Massolini Silva <massolini@gmail.com>
2022-09-08 11:31:28 +0200

    modificacoes de nomes e adicao de arquivo do dia 6

---
+++ Day6/day6_reeborg.py
@@ -0,0 +1,12 @@
+'''
+Day 6 was based on while we made most of the challanges on this web site
+
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+as you can check there is some specific challenges on the directory 
+
+
+
+
+
+'''
