c7c7be86dddf077f96146e26b8783c719de6e1da
Mauro Massolini Silva <massolini@gmail.com>
2022-09-14 12:08:00 +0200

    criado arquivo de instrucoes e arquivo inicial em area calc directory

--- Day8 Cipher - funcions with inputs/function_with_inputs.py
+++ Day8 Cipher - funcions with inputs/function_with_inputs.py
@@ -1,8 +1,17 @@
-def greet(name):
+# def greet(name):
     
-    print(f"\nWelcome {name} to Cipher program!\nWith this aplication you make your personal conversation safer!\n")
+#     print(f"\nWelcome {name} to Cipher program!\nWith this aplication you make your personal conversation safer!\n")
 
-greet("Mauro")
-    
+# greet("Mauro")
+   
+# Functions with more than 1 input
+def greet_with (name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
 
+# If you switch the order it can put in the wrong place
+# greet_with("Mauro", "Pamplona")
+
+# to be at the correct location must have a keyword arguments / by using this you don´t have the problem of missplace by changin the order
+greet_with(name="Mauro", location="Pamplona")
     
